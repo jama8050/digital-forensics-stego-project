a3513050d2d3a89d56414533adc6e429df994a9a
Jacob Malcy <jama8050@colorado.edu>
2019-11-19 02:33:51 -0700

    add file stat todo

--- stego.py
+++ stego.py
@@ -11,6 +11,7 @@ EMBED_STR = "_embedded"  # Add onto the end of a file name that we modify
 # The following numbers were randomly generated
 START_MARKER = b'\x4f\x85\x61\x3a\x57\x41\x1d\xea\xc8\xa8'  # Custom start marker
 END_MARKER = b'\x4f\x85\x61\x3a\x57\x41\x1d\xa8\xc8\xea'  # Custom end marker (start_marker with last 3 bytes reversed)
+# TODO: Add file statistics function (prints size, name, md5, etc.)
 
 def init():
     # Setup argument parsing
