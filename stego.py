cc26e75056beb5bc9c26b4025c8273cb4ae2d0ef
Jacob Malcy <jama8050@colorado.edu>
2019-11-19 02:34:49 -0700

    adding appending function

--- stego.py
+++ stego.py
@@ -13,6 +13,18 @@ START_MARKER = b'\x4f\x85\x61\x3a\x57\x41\x1d\xea\xc8\xa8'  # Custom start marke
 END_MARKER = b'\x4f\x85\x61\x3a\x57\x41\x1d\xa8\xc8\xea'  # Custom end marker (start_marker with last 3 bytes reversed)
 # TODO: Add file statistics function (prints size, name, md5, etc.)
 
+
+# Append the secret message to a file
+def append(file_name, message):
+    message_to_write = START_MARKER + str.encode(message, ENCODING) + END_MARKER
+    last_dot = file_name.rfind('.')
+    new_name = file_name[:last_dot] + EMBED_STR + file_name[last_dot:]
+
+    with open(file_name, 'rb') as image, open(new_name, 'wb') as new_image:
+        new_image.write(image.read() + message_to_write)
+
+
+# Sets up argument parsing with some error checking
 def init():
     # Setup argument parsing
     parser = ArgumentParser(description="Steganography embedding/extracting program in Python 3.7",
@@ -34,7 +46,7 @@ def main():
 
     if parsed.secret:
         # In insert mode
-        pass
+        append(parsed.carrier, parsed.secret)
 
 
 if __name__ == "__main__":
