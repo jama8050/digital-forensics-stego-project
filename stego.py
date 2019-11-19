236d1bfedd4a80d955667e75953baf75c14d3480
Jacob Malcy <jama8050@colorado.edu>
2019-11-19 02:35:02 -0700

    adding extraction function

--- stego.py
+++ stego.py
@@ -24,6 +24,37 @@ def append(file_name, message):
         new_image.write(image.read() + message_to_write)
 
 
+# Extract the message
+def extract(file_name):
+    with open(file_name, 'rb') as image:
+        data = image.read()
+
+    # Locate message via custom magic numbers
+    start_index = data.rfind(START_MARKER)
+    end_index = data.rfind(END_MARKER)
+
+    if start_index == -1 or end_index == -1:
+        raise RuntimeError("Starting and/or ending magic numbers could not be found")
+
+    i = file_name.rfind(EMBED_STR)
+    if i != -1:
+        new_name = file_name[:i] + "_unembedded" + file_name[i + len(EMBED_STR):]
+    else:
+        i = file_name.rfind('.')
+        if i == -1:
+            i = len(file_name)
+        new_name = file_name[:i] + "_unembedded" + file_name[i:]
+
+    # decoded, secret message
+    str_return = (data[start_index + len(START_MARKER):end_index]).decode(ENCODING)
+
+    # Write the carrier file without the secret message inside it
+    with open(new_name, "wb") as image:
+        image.write(data[:start_index] + data[end_index + len(END_MARKER):])
+
+    return str_return
+
+
 # Sets up argument parsing with some error checking
 def init():
     # Setup argument parsing
@@ -47,6 +78,9 @@ def main():
     if parsed.secret:
         # In insert mode
         append(parsed.carrier, parsed.secret)
+    else:
+        # in extract mode
+        print('Secret message: "{}"'.format(extract(parsed.carrier)))
 
 
 if __name__ == "__main__":
